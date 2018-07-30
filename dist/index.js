119304574edc33006790a485a13b972a65ef3e28
Chris Drackett <chris@drackett.com>
2018-07-30 15:28:33 -0700

    add build files

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+exports.__esModule = true;
+exports.Store = exports.State = undefined;
+
+var _State2 = require('./State');
+
+var _State3 = _interopRequireDefault(_State2);
+
+var _Store2 = require('./Store');
+
+var _Store3 = _interopRequireDefault(_Store2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.State = _State3.default;
+exports.Store = _Store3.default;
